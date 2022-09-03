1bd0df05673629fe21ec4eb0a31ab05d6dc16fdb
koonwen <koonwen@gmail.com>
2022-09-03 19:02:10 +0800

    More renaming

--- hello-world/main.ml
+++ hello-world/main.ml
@@ -1,8 +1,8 @@
-external stdio_write : Cstruct.buffer -> int -> int = "caml_stdio_write"
+external riot_stdio_write : Cstruct.buffer -> int -> int = "caml_stdio_write"
 
 let () =
   let mystring = "Hello from OCaml on RIOT!" in
   let cs = Cstruct.of_string mystring in
   let buf = Cstruct.to_bigarray cs in
-  let bytes_written = stdio_write buf (String.length mystring) in
+  let bytes_written = riot_stdio_write buf (String.length mystring) in
   assert (bytes_written = 25);
